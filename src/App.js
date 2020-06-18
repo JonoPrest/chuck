c8b04810ef64c11bc86ea8f25a984260a53d8829
Jono Prest <jjprest@gmail.com>
2020-06-18 17:37:11 +0200

    Chcuk Norris jokes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Joke from './Joke';
+import Norris from './Norris.jpg';
 import './App.css';
+import 'tachyons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      joke: 'click the green thing above'    
+    }
+
+  }
+
+
+  
+
+  
+
+  render() {
+
+    const chuckJoke = () => {
+        fetch('https://api.chucknorris.io/jokes/random')
+        .then(response=> response.json())
+        .then(result=> this.setState({joke: result.value}))
+    }
+      
+
+    return(  
+    <div className="tc fl w-100 pa2">
+      <img className="" src={Norris}/>
+      <div className="center fl w-100 pa3">
+        <button className="bg-dark-green white courier" onClick={chuckJoke}>Tell Me a Chuck Norris Joke PLZ...</button>
+      </div> 
+      <div className="center mw5 pa3" >
+        <Joke text={this.state.joke} />
+      </div>
     </div>
-  );
+    );
+    
+  }
+
 }
+
+
+    
 
 export default App;
---
+++ src/Joke.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+const Joke = ({text}) => {
+
+		
+	return (
+	<p className="f3 courier b" >{text}</p>
+	);
+
+		
+
+}
+
+export default Joke;
+
