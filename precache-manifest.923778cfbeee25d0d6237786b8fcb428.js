236e14a288187c7e3f4a5403cfa5be5d7988de76
Jono Prest <jjprest@gmail.com>
2020-06-18 17:40:05 +0200

    Updates

---
+++ precache-manifest.923778cfbeee25d0d6237786b8fcb428.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80fce34d26538d8ea7b7f12313c335cf",
+    "url": "/chuck/index.html"
+  },
+  {
+    "revision": "72059b59ab7b13b8e69f",
+    "url": "/chuck/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ee92d03a0c7f7ddf8d10",
+    "url": "/chuck/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "72059b59ab7b13b8e69f",
+    "url": "/chuck/static/js/2.561cc180.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chuck/static/js/2.561cc180.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee92d03a0c7f7ddf8d10",
+    "url": "/chuck/static/js/main.e0135045.chunk.js"
+  },
+  {
+    "revision": "cbb70fdb6f4daba8405f",
+    "url": "/chuck/static/js/runtime-main.3b11d4e6.js"
+  },
+  {
+    "revision": "f3e4a6613374f1201404f0bff94b2108",
+    "url": "/chuck/static/media/Norris.f3e4a661.jpg"
+  }
+]);
